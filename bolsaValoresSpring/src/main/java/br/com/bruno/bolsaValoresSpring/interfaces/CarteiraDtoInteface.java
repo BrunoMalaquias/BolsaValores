d5d6bc9d79c5a526bcdc3fc40debbc43eab579ac
Bruno Malaquias <80713294+BrunoMalaquias@users.noreply.github.com>
2021-09-15 19:55:00 -0300

    Inclusão das Interfaces

---
+++ bolsaValoresSpring/src/main/java/br/com/bruno/bolsaValoresSpring/interfaces/CarteiraDtoInteface.java
@@ -0,0 +1,11 @@
+package br.com.bruno.bolsaValoresSpring.interfaces;
+
+public interface CarteiraDtoInteface {
+	Integer getQtd();
+	//String getNome();
+	String getPreco_medio();
+	String getValor_investido();
+	
+	String getCodigo_ativo();
+
+}
